f54f4950a53e156275a353be5df8ca633aacae52
ChrisDerrig <derrigc@gmail.com>
2015-07-31 13:22:20 -0400

    Added Support for "pill" tabs and callback functions
    Supporting data-toggle="pill" as well, and
    data-callback="yourjavascriptfunction()" to allow for a callback once
    your url is lazy loaded.

--- Scripts/MT.BootstrapTabsLazyloader.js
+++ Scripts/MT.BootstrapTabsLazyloader.js
@@ -9,13 +9,15 @@ Adds Lazy loading feature to bootstrap tabs
 */
 
 
-$(function() {
+$(function () {
     $(document).on('shown.bs.tab', '.nav-tabs.lazyload a[data-toggle="tab"]:not(.loaded)', function (e) {
         var loader = '<div class="text-center"><i class="fa fa-spin fa-spinner fa-2x text-muted"></i></div>';
 
         var pane = $(e.target).attr('href');
 
         var url = $(e.target).data('url');
+
+        var callback = $(e.target).data('callback');
 
         if (url) {
             $(pane).html(loader);
@@ -28,6 +30,40 @@ $(function() {
             .fail(function () {
                 var alertDiv = '<div class="alert alert-danger"><i class="fa fa-exclamation-triangle"></i> Error!!!</div>';
                 $(pane).html(alertDiv);
+            })
+            .done(function () {
+                if (callback) {
+                    eval(callback);
+                }
+            });
+        }
+    });
+
+    $(document).on('shown.bs.tab', '.nav-pills.lazyload a[data-toggle="pill"]:not(.loaded)', function (e) {        
+        var loader = '<div class="text-center"><i class="fa fa-spin fa-spinner fa-2x text-muted"></i></div>';
+
+        var pane = $(e.target).attr('href');
+
+        var url = $(e.target).data('url');
+
+        var callback = $(e.target).data('callback');
+
+        if (url) {
+            $(pane).html(loader);
+
+            var caller = $(this);
+            $.get(url, function (data) {
+                $(pane).html(data);
+                $(caller).addClass('loaded');
+            })
+            .fail(function () {
+                var alertDiv = '<div class="alert alert-danger"><i class="fa fa-exclamation-triangle"></i> Error!!!</div>';
+                $(pane).html(alertDiv);
+            })
+            .done(function () {
+                if (callback) {
+                    eval(callback);
+                }
             });
         }
     });
